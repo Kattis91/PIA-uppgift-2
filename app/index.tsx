516228313d87b17b388ce071f5c46f4b31bbdded
Kattis91 <katya.svedmark@gmail.com>
2024-09-25 12:26:27 +0200

    Add backgroundColor and some style to all the buttons

--- app/index.tsx
+++ app/index.tsx
@@ -20,27 +20,40 @@ export default function Index() {
       style={{
         flex: 1,
         alignItems: "center",
+        backgroundColor: "#FFFFC2"
       }}
     >
       <Text style={{ fontSize: 75 }}>{counter}</Text>
 
-      <Button
-        title="PLUS"
-        onPress={addition}
-      />
+      <View style={{ flexDirection: "row" }}>
 
-      <Button
-        title="MINUS"
-        onPress={subtraktion}
-      />
+        <View style={{ backgroundColor: "green", borderRadius: 10, marginRight: 10, marginTop: 20, padding: 5 }}>
+          <Button
+            title="PLUS"
+            color="white"
+            onPress={addition}
+          />
+        </View>
+
+        <View style={{ backgroundColor: "blue", borderRadius: 10, marginLeft: 10, marginTop: 20, padding: 5 }}>
+          <Button
+            title="MINUS"
+            color="white"
+            onPress={subtraktion}
+          />
+        </View>
+      </View>
 
       {(counter > 0) &&
-        <Button
-          title="RESET"
-          onPress={() => {
-            setCounter(0);
-          }}
-        />
+        <View style={{ backgroundColor: "red", borderRadius: 10, marginTop: 20, padding: 5 }}>
+          <Button
+            title="RESET"
+            color= "white"
+            onPress={() => {
+              setCounter(0);
+            }}
+          />
+        </View>
       }
 
     </View>
